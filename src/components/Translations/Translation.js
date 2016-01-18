b1cba3da21846040258227e287720516e80f23b9
djfm <fm.de.jouvencel@gmail.com>
2016-01-18 13:43:50 +0100

    // save widget

--- src/components/Translations/Translation.js
+++ src/components/Translations/Translation.js
@@ -13,25 +13,53 @@ export default class Domain extends Component {
 
   constructor() {
     super();
-    _.bindAll(this, 'translationChanged');
+    _.bindAll(this, 'translationChanged', 'saveTranslation', 'handleKey');
   }
 
   componentWillMount() {
-    this.setState({ translation: this.props.translation });
+    this.setState({ translation: this.props.translation, dirty: false });
   }
 
   translationChanged(event) {
-    this.setState({ translation: event.target.value });
+    this.setState({ translation: event.target.value, dirty: true });
+  }
+
+  handleKey(event) {
+    if (event.which === 83 /* s */) {
+      if (event.getModifierState('Control')) {
+        event.preventDefault();
+        this.saveTranslation();
+      }
+    }
+  }
+
+  saveTranslation() {
+    this.setState({ dirty: false });
+  }
+
+  renderSaveWidget() {
+    if (this.state.dirty) {
+      return <a onClick={this.saveTranslation}><i className="material-icons">save</i></a>;
+    } else {
+      return <i className="material-icons">done</i>;
+    }
   }
 
   render() {
     return (
       <div className={s['translation-wrapper']}>
         <div className={s.string}>
-          {this.props.string}
+          <span>{this.props.string}</span>
         </div>
         <div className={s.translation}>
-          <textarea onChange={this.translationChanged} value={this.state.translation}/>
+          <div className={s.row}>
+            <div className={s['col-xs-11']}>
+              <textarea onChange={this.translationChanged} value={this.state.translation} onKeyDown={this.handleKey}/>
+            </div>
+            <div className={s['col-xs-1']}>
+              {this.renderSaveWidget()}
+            </div>
+          </div>
         </div>
       </div>
     );
