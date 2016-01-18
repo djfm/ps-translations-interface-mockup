c416dfb70168dfaf9ad068e5c7a663b1ea30a574
djfm <fm.de.jouvencel@gmail.com>
2016-01-18 08:54:54 +0100

    Added translations inputs

--- src/components/Translations/Translation.js
+++ src/components/Translations/Translation.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import withStyles from '../../decorators/withStyles';
 
 import s from './Translations.scss';
+import _ from 'underscore';
 
 @withStyles(s)
 export default class Domain extends Component {
@@ -10,14 +11,27 @@ export default class Domain extends Component {
     translation: PropTypes.string,
   };
 
+  constructor() {
+    super();
+    _.bindAll(this, 'translationChanged');
+  }
+
+  componentWillMount() {
+    this.setState({ translation: this.props.translation });
+  }
+
+  translationChanged(event) {
+    this.setState({ translation: event.target.value });
+  }
+
   render() {
     return (
-      <div className={s.row}>
-        <div className={s['col-lg-6']}>
+      <div className={s['translation-wrapper']}>
+        <div className={s.string}>
           {this.props.string}
         </div>
-        <div className={s['col-lg-6']}>
-          {this.props.translation}
+        <div className={s.translation}>
+          <textarea onChange={this.translationChanged} value={this.state.translation}/>
         </div>
       </div>
     );
